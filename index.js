a6053f5d85cc4f8d8dbcf7530a707533d95fa843
LeeYunHak <sza0203@naver.com>
2022-01-30 17:22:19 +0900

    회원가입 기능, User 스키마 수정

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
 require("dotenv").config();
-const mongoose = require("mongoose");
 
+const { User } = require("./models/User");
+// application / x-www-form-urlencoded 분석
+app.use(bodyParser.urlencoded({ extended: true }));
+// aplication/json 분석
+app.use(bodyParser.json());
+
+const mongoose = require("mongoose");
 mongoose
   .connect(process.env.DB_URL, {
     useNewUrlParser: true,
@@ -13,5 +20,17 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World"));
+
+app.post("/register", (req, res) => {
+  // 회원 가입 할때 필요한 정보들을 client에서 가져오고
+  // 데이터베이스에 넣어줌
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  }); // 정보들이 유저 모델에 저장
+});
 
 app.listen(port, () => console.log(`app listening on port ${port}`));
