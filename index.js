c2d370bee54bbb4bdff65badc86bb342886b3c0c
LeeYunHak <sza0203@naver.com>
2022-01-30 23:25:05 +0900

    Auth 기능 완성

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const port = process.env.PORT;
 
+const { auth } = require("./middleware/auth");
 const { User } = require("./models/User");
 // application / x-www-form-urlencoded 분석
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -22,7 +23,7 @@ mongoose
 
 app.get("/", (req, res) => res.send("Hello World"));
 // 가입
-app.post("/register", (req, res) => {
+app.post("/api/users/register", (req, res) => {
   // 회원 가입 할때 필요한 정보들을 client에서 가져오고
   // 데이터베이스에 넣어줌
   const user = new User(req.body);
@@ -35,7 +36,7 @@ app.post("/register", (req, res) => {
 });
 
 // 로그인
-app.post("/login", (req, res) => {
+app.post("/api/users/login", (req, res) => {
   // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
   User.findOne({ email: req.body.email }, (err, user) => {
     if (!user) {
@@ -63,6 +64,23 @@ app.post("/login", (req, res) => {
           .json({ loginSuccess: true, userId: user._id });
       });
     });
+  });
+});
+
+// role 1 어드민   role 2 특정 부서 어드민
+// role 0 -> 일반유저  role 0이 아니면 관리자
+
+app.get("/api/users/auth", auth, (req, res) => {
+  // 여기 까지 미들웨어를 통과해 왔다는 얘기는 Authentication 이 True 라는 말.
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
   });
 });
 
