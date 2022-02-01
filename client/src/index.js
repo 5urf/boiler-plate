f8af1b523de1d96de94771c654a223187f5ede9b
LeeYunHak <sza0203@naver.com>
2022-02-01 21:33:13 +0900

    로그인 페이지 완성 및 Redux 세팅

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,28 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "antd/dist/antd.css";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+import Reducer from "./_reducers";
+// 그냥 stroe는 객체 밖에 못받기 때문에 Promise와 function도 받기위해 미들웨어와 함께 만들어줌
+const createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk
+)(createStore);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider
+      store={createStoreWithMiddleware(
+        Reducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+      )}
+    >
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
